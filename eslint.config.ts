c459ceb361922e93ad53748fd601a42b34db6bc2
Martin Bullman <martin.j.bullman@gmail.com>
2025-08-09 21:07:34 +0300

    Adds ESLint configuration for the project

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+// import eslintConfigPrettier from "eslint-config-prettier/flat";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        ignores: ["dist/"]
+    },
+    {
+        files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+        ],
+    },
+]);
+
